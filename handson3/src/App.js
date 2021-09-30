bfc1dcda95f3634d7846a774cfec78df4aecc477
Sam Tan <samtisawesomest@gmail.com>
2021-09-30 04:34:18 +0000

    Added ticklebox js

---
+++ handson3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NumberBox from './NumberBox';
+import TickleBox from './TickleBox';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <NumberBox initialValue={10}/> */}
+      <TickleBox text="I'm elmo!"/>
+    </div>
+  );
+}
+
+export default App;
